8cd2126b029c09ad62e25c944bbac195e91f7c93
ifreeky <teng.dang@wormpex.com>
2020-05-27 22:58:49 +0800

    后续遍历

--- src/com/ifreeky/learn/test/tree/binary/traverse/preorder/TraverseTest.java
+++ src/com/ifreeky/learn/test/tree/binary/traverse/preorder/TraverseTest.java
@@ -44,5 +44,6 @@ public class TraverseTest {
 
     public void testPostOrder() {
         PostOrder postOrder = new PostOrder();
+        postOrder.unRecursive(root);
     }
 }
--- src/com/ifreeky/learn/tree/binary/traverse/postorder/PostOrder.java
+++ src/com/ifreeky/learn/tree/binary/traverse/postorder/PostOrder.java
@@ -2,14 +2,18 @@ package com.ifreeky.learn.tree.binary.traverse.postorder;
 
 import com.ifreeky.learn.tree.binary.base.Node;
 
+import java.util.Objects;
+import java.util.Stack;
+
 /**
+ * @link https://zhuanlan.zhihu.com/p/80578741
  * @author ifreeky
  * @since 15 五月 2020
  */
 public class PostOrder {
 
     public void recursive(Node root) {
-        if (null == root){
+        if (null == root) {
             return;
         }
         recursive(root.getLeft());
@@ -17,6 +21,35 @@ public class PostOrder {
         print(root);
     }
 
+    public void unRecursive(Node root) {
+        if (Objects.isNull(root)) {
+            return;
+        }
+
+        Stack<Node> stack = new Stack<>();
+        Node cur = root;
+        Node per = null;
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.getLeft();
+            }
+
+            if (!stack.isEmpty()) {
+                cur = stack.pop();
+                if (cur.getRight() == null || per == cur.getRight()) {
+                      print(cur);
+                      per = cur;
+                      cur = null;
+                } else {
+                    stack.push(cur);
+                    cur = cur.getRight();
+                }
+
+            }
+        }
+
+    }
 
     private void print(Node node) {
         System.out.println(node.getData());
