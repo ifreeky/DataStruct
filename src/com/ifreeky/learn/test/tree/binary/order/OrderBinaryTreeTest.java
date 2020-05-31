544274cc09f52eddf8c97bdc10ecb64af9c362b7
ifreeky <teng.dang@wormpex.com>
2020-05-31 22:01:28 +0800

    顺序存储二叉树

---
+++ src/com/ifreeky/learn/test/tree/binary/order/OrderBinaryTreeTest.java
@@ -0,0 +1,23 @@
+package com.ifreeky.learn.test.tree.binary.order;
+
+import com.ifreeky.learn.tree.binary.order.OrderBinaryTree;
+
+/**
+ * @author ifreeky
+ * @since 31 五月 2020
+ */
+public class OrderBinaryTreeTest {
+
+    public static void main(String[] args) {
+        OrderBinaryTree preOrderBinaryTree = new OrderBinaryTree(new int[]{1, 2, 3, 4, 5, 6, 7});
+        preOrderBinaryTree.preOrder();
+
+        OrderBinaryTree midOrderBinaryTree = new OrderBinaryTree(new int[]{1, 2, 3, 4, 5, 6, 7});
+        midOrderBinaryTree.midOrder();
+
+        OrderBinaryTree postOrderBinaryTree = new OrderBinaryTree(new int[]{1, 2, 3, 4, 5, 6, 7});
+        postOrderBinaryTree.postOrder();
+
+
+    }
+}
